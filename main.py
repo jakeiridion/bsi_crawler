9c13be685e4103087cf25d1a0e8a5ef4de3f8edc
jakeiridion <emil.ej.jeleskovic@gmail.com>
2019-10-12 10:21:33 +0200

    requests works

--- main.py
+++ main.py
@@ -2,3 +2,23 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
+article = {}
+user_agent = {'User-agent': 'Mozilla/5.0'}
+url = "https://www.bsi.bund.de/DE/Themen/ITGrundschutz/ITGrundschutzKompendium/bausteine/SYS/SYS_1_1_Allgemeiner_Server.html;jsessionid=B0793904D3CF0165506E43AFEDBDC3BE.1_cid360"
+
+
+class CrawledArticle():
+    def __init__(self, title, txt):
+        self.title = title
+        self.txt = txt
+
+
+class Fetcher():
+    def fetch(self):
+        time.sleep(1)
+        r = requests.get(url, headers=user_agent)
+        doc = BeautifulSoup(r.text, "html.parser")
+
+
+f = Fetcher()
+f.fetch()
