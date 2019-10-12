da0aadcbb9a83637ed47f29fce1c64b0062cfe2d
jakeiridion <emil.ej.jeleskovic@gmail.com>
2019-10-12 22:50:13 +0200

    usefull inf now in list

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 
 article = {}
 user_agent = {'User-agent': 'Mozilla/5.0'}
-url = "https://www.bsi.bund.de/DE/Themen/ITGrundschutz/ITGrundschutzKompendium/bausteine/SYS/SYS_1_1_Allgemeiner_Server.html;jsessionid=B0793904D3CF0165506E43AFEDBDC3BE.1_cid360"
+url = "https://www.bsi.bund.de/DE/Themen/ITGrundschutz/ITGrundschutzKompendium/bausteine/SYS/SYS_4_1_Drucker,_Kopierer_und_Multifunktionsger%C3%A4te.html"
 
 
 class CrawledArticle():
@@ -18,6 +18,31 @@ class Fetcher():
         time.sleep(1)
         r = requests.get(url, headers=user_agent)
         doc = BeautifulSoup(r.text, "html.parser")
+
+        ges = doc.find_all(["h4", "p", "h2", "li"])  # ,li für listen
+        fin = []
+        fin2 = []
+
+        for i in ges:
+            if "navToTop" in str(i) or "class=" in str(i) or "a href" in str(i):
+                continue
+
+            fin.append(str(i))
+
+        o = 0
+        for i in fin:
+            if (i[0:3] == "<p>" or i[0:4] == "<h2>") and o == 0:
+                continue
+
+            if i[0:4] == "<h4>":
+                o = 1
+
+            fin2.append(i)
+
+        # make all the useless stuff pop
+
+        for i in fin2:
+            print(i)
 
 
 f = Fetcher()
